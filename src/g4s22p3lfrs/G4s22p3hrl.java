b3a470b652d2a086a6a9d21016ae14d659d6512c
luiss <luiss@LAPTOP-OI16B963>
2021-04-23 18:12:50 -0500

    Practica3

---
+++ src/g4s22p3lfrs/G4s22p3hrl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p3lfrs;
+
+/**
+ *
+ * @author Ana Cecilia
+ */
+public class G4s22p3hrl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+       
+  
+        }
+    
+
+    
+}
